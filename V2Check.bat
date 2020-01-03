fc1bda80cf40e822086f2f65946317c85a21eda0
Andrew Waugh <andrew.waugh@prov.vic.gov.au>
2020-01-03 12:52:13 +1100

    Redid the Bat file to be simpler and more portable

--- V2Check.bat
+++ V2Check.bat
@@ -1,5 +1,5 @@
 @echo off
-rem set code="C:\Users\Andrew\Documents\Work\VERS-2015\VPA"
-set code="J:\PROV\TECHNOLOGY MANAGEMENT\Application Development\VERS\VERS-1999\V2Check"
+set code="C:\Users\Andrew\Documents\Work\VERSCode\V2Check"
+rem set code="J:\PROV\TECHNOLOGY MANAGEMENT\Application Development\VERS\VERSCode\V2Check"
 set versclasspath=%code%/dist/*
 java -classpath %versclasspath% VEOCheck.VEOCheck -all %*
