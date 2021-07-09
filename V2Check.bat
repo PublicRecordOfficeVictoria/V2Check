3b97d623960065b2ebdb1e1da981dd9642ea8ebb
Andrew Waugh <andrew.waugh@prov.vic.gov.au>
2021-07-09 16:35:04 +1000

    Dummy commit

--- V2Check.bat
+++ V2Check.bat
@@ -9,3 +9,4 @@ if exist "J:/PROV/TECHNOLOGY MANAGEMENT/Application Development/VERS/VERSCode" (
 	set code="C:/Users/Andrew/Documents/Work/VERSCode"
 )
 java -classpath %code%/V2Check/dist/* VEOCheck.VEOCheck -values -signatures -f %code%/VERSCommon/VERSSupportFiles/validLTSF.txt -dtd %code%/VERSCommon/VERSSupportFiles/vers2.dtd %*
+rem nothing
